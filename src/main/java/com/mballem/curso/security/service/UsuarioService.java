b2bc5cc7364a15be7ef89141fca9ce58320c0b9f
will henrique <willhc1984@gmail.com>
2023-07-04 14:13:06 -0300

    Lista de usuarios

--- src/main/java/com/mballem/curso/security/service/UsuarioService.java
+++ src/main/java/com/mballem/curso/security/service/UsuarioService.java
@@ -1,8 +1,12 @@
 package com.mballem.curso.security.service;
 
 import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,6 +15,8 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.mballem.curso.security.datatables.Datatables;
+import com.mballem.curso.security.datatables.DatatablesColunas;
 import com.mballem.curso.security.domain.Perfil;
 import com.mballem.curso.security.domain.Usuario;
 import com.mballem.curso.security.repository.UsuarioRepository;
@@ -20,6 +26,9 @@ public class UsuarioService implements UserDetailsService{
 	
 	@Autowired
 	private UsuarioRepository repository;
+	
+	@Autowired
+	private Datatables datatables;
 	
 	@Transactional(readOnly = true)
 	public Usuario buscarPorEmail(String email) {
@@ -41,6 +50,17 @@ public class UsuarioService implements UserDetailsService{
 			authorities[i] = perfis.get(i).getDesc();
 		}
 		return authorities;
+	}
+	
+	@Transactional(readOnly = true)
+	public Map<String, Object> buscarTodos(HttpServletRequest request) {
+		System.out.println(request);
+		datatables.setRequest(request);
+		datatables.setColunas(DatatablesColunas.USUARIOS);
+		Page<Usuario> page = datatables.getSearch().isEmpty()
+				? repository.findAll(datatables.getPageable())
+				: repository.findByEmailOrPerfil(datatables.getSearch(), datatables.getPageable());
+		return datatables.getResponse(page);
 	}
 
 }
--- src/main/java/com/mballem/curso/security/web/controller/UsuarioController.java
+++ src/main/java/com/mballem/curso/security/web/controller/UsuarioController.java
@@ -1,20 +1,39 @@
 package com.mballem.curso.security.web.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.mballem.curso.security.domain.Usuario;
+import com.mballem.curso.security.service.UsuarioService;
 
 @Controller
 @RequestMapping("u")
 public class UsuarioController {
 	
+	@Autowired
+	private UsuarioService service;
+	
+	//abrir cadastro de usuarios (medico/paciente/admin)
 	@GetMapping("/novo/cadastro/usuario")
-	public String abrirPorAdminParaAdminMedicoUsuario(Usuario usuario) {
+	public String cadastroPorAdminParaAdminMedicoPaciente(Usuario usuario) {
 		return "usuario/cadastro";
 	}
 	
+	//abrir lista de usuarios
+	@GetMapping("/lista")
+	public String listarUsuarios() {
+		return "usuario/lista";
+	}
 	
+	//abrir lista de usuarios
+	@GetMapping("/datatables/server/usuarios")
+	public ResponseEntity<?> listarUsuariosDatatables(HttpServletRequest request) {
+		return ResponseEntity.ok(service.buscarTodos(request));
+	}
 
 }
